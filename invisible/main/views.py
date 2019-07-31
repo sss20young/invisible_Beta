fb35967cc788fc8af14fb8056c8a826c71813791
seunghee9941 <seunghee9941@gmail.com>
2019-08-01 07:55:04 +0900

    main 0801

--- invisible/main/views.py
+++ invisible/main/views.py
@@ -5,4 +5,9 @@ def main(request):
     return render(request, 'main.html')
 
 def result(request):
-    return render(request, 'best_lecture_result.html')
+    if request.method == 'GET':
+        subject = request.GET['subject']
+        grade = request.GET['grade']
+        study = request.GET['study']
+        teacher = request.GET['teacher']
+    return render(request, 'best_lecture_result.html', {'subject':subject, 'grade':grade, 'study':study, 'teacher':teacher})
