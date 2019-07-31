6730091ff2ad23d570ef85f6dc4b24fd42c2551f
invisibleWarehouse <officialinvisible030@gmail.com>
2019-07-31 18:16:13 +0000

    connect mysql

--- invi_project/settings.py
+++ invi_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'invi_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'invisible',
+        'USER': 'invi_back',
+        'PASSWORD': 'hardCarryBack',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
