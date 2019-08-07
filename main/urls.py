4d7c12a245830642bdb9e6b30af9ec55669083c5
seunghee9941 <seunghee9941@gmail.com>
2019-08-08 08:46:47 +0900

    메인 이모저모 승희

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'), #메인 페이지
     path('result', views.result, name='result'), #메인 폼 결과 페이지
-    #path('result/highhits/', views.result_highhits, name="result_highhits"), #메인 폼 결과 조회수 정렬 페이지
-    #path('result/hightprice/', views.result_highprice, name="result_highprice"), # 메인 폼 결과 높은가격순 정렬 페이지
-    #path('result/lowprice/', views.result_lowprice, name="result_lowprice"), # 메인 폼 결과 낮은가격순 정렬 페이지
+    path('result/highhits/', views.result_highhits, name="result_highhits"), #메인 폼 결과 조회수 정렬 페이지
+    path('result/hightprice/', views.result_highprice, name="result_highprice"), # 메인 폼 결과 높은가격순 정렬 페이지
+    path('result/lowprice/', views.result_lowprice, name="result_lowprice"), # 메인 폼 결과 낮은가격순 정렬 페이지
 ]
