e6a594f065b157188691df8ee88c7bcb7f78cfbf
eunjungleecub <asaprocky123@likelion.org>
2019-08-09 07:33:36 +0900

    카테고리 css 끝남 & return 아이템 나옴

--- invi_project/settings.py
+++ invi_project/settings.py
@@ -147,6 +147,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'invi_app/static'),
     os.path.join(BASE_DIR, 'main/static'),
+    os.path.join(BASE_DIR, 'categories/static'),
 ]
 
 STATIC_ROOT = os.path.join (BASE_DIR, 'static')
